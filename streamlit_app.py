554ac2b35a9a4fd0619ebfea7af7ffaaa77a0b7e
pvimalacode <85435096+pvimalacode@users.noreply.github.com>
2024-11-26 21:49:02 +0000

    added to pandas df

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,10 @@ def clear_text():
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
+pd_df=my_dataframe.to_pandas()
+st.data_frame(pd_df)
 st.stop()
 
 
