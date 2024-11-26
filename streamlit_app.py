caa913eaac65ea9f00852501968f6fcba16978a4
pvimalacode <85435096+pvimalacode@users.noreply.github.com>
2024-11-26 21:51:10 +0000

    checking search on value

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,6 +55,10 @@ if ingredients_string and name_on_order:
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
                 values ('""" + ingredients_string + """','""" +  name_on_order +  """');"""
     for ingredient in ingredients_list:
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
         st.subheader(ingredient + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ ingredient)
         #st.write("https://my.smoothiefroot.com/api/fruit/"+ ingredient)
